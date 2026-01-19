c265c6679bc6781be0ee38ed17be2dedd37a6ae3
Mashrur Ahmed <mashrurahmed500@gmail.com>
2026-01-19 18:04:55 +0600

    SOUNDS ADDED IN THE FOLDER

---
+++ Assets/Script/audioManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+
+    [Header("music")]
+    [SerializeField] AudioSource background;   // already done
+    [SerializeField] AudioSource SFX;  // new one for fail panel
+
+
+    [Header("Music")]
+    public AudioClip menuMusic;
+
+    [Header("sfx")]
+    public AudioClip win;
+    public AudioClip lose;
+
+
+
+
+
+
+
+
+
+
+}
